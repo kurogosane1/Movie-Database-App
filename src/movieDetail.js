2be067771e2e6ce8a61195a219964a335d79bad4
kurogosane1 <magnite@hotmail.com>
2018-01-19 22:32:24 -0600

    Adding the remaining files

---
+++ src/movieDetail.js
@@ -0,0 +1,73 @@
+/* eslint react/no-did-mount-set-state: 0 */
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import './App.css';
+import Movie from './movie';
+import { Poster } from './movie';
+import Overdrive from 'react-overdrive';
+
+
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
+class MovieDetail extends Component {
+  state = {
+    movie: {},
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=e737851cdf293728dce4a284c5cc83e8&language=en-US`);
+      const movie = await res.json();
+      //eslint-disable-line
+      this.setState({
+        movie,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    const { movie } = this.state;
+    return (
+      <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title}>
+        <MovieInfo>
+          <Overdrive id={movie.id}>
+          <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+        </Overdrive>
+          <div>
+            <h1>{movie.title}</h1>
+            <h3>{movie.release_date}</h3>
+            <p>{movie.overview}</p>
+          </div>
+        </MovieInfo>
+      </MovieWrapper>
+    );
+  }
+}
+
+export default MovieDetail;
+
+const MovieWrapper = styled.div`
+    position: relative;
+    padding-top: 50vh;
+    background: url(${props => props.backdrop}) no-repeat;
+    background-size: cover;
+`;
+
+
+const MovieInfo = styled.div`
+    background: white;
+    text-align: left;
+    padding: 2rems 10%;
+    display: flex;
+
+    >div {
+        margin-left: 20px;
+    }
+    img {
+        position: relative;
+        top: -5rem;
+        
+    }
+`;
---
+++ src/moviesList.js
@@ -0,0 +1,44 @@
+/* eslint react/no-did-mount-set-state: 0 */
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+import './App.css';
+import Movie from './movie';
+
+
+class MoviesList extends PureComponent {
+  state = {
+    movies: [],
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=e737851cdf293728dce4a284c5cc83e8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      //eslint-disable-line
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <MovieGrid>
+          {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </MovieGrid>
+    );
+  }
+}
+
+export default MoviesList;
+
+const MovieGrid = styled.div`
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: repeat(5, 1fr);
+  grid-row-gap: 1rem;
+`;
+
+
